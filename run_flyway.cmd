f9f70592e0e9451adcd9d95d0ba56a26caf859b3
Dale Highfill <dghighfill@gmail.com>
2023-07-30 07:38:56 -0500

    Converted Database to SQLite

--- run_flyway.cmd
+++ run_flyway.cmd
@@ -2,4 +2,4 @@ ECHO OFF
 set FLYWAY_HOME=I:\desktop-setup\test\DevTools\flyway-9.4.0
 
 @REM This command will allow paramters to be passed such as info or migrate
-%FLYWAY_HOME%/flyway.cmd -configFiles="./conf/flyway.conf" %1 %2 %3 %4
+%FLYWAY_HOME%\flyway.cmd -configFiles="./conf/flyway_sqlite.conf" %1 %2 %3 %4
--- sql/V1.1__create_customer_table.sql
+++ sql/V1.1__create_customer_table.sql
@@ -2,6 +2,7 @@ CREATE TABLE customer (
     id int NOT NULL,
     first_name  varchar(35) NOT NULL,
     last_name   varchar(35) NOT NULL,
+    middle_initial varchar(1),
     city        varchar(50),
     state       varchar(2),
     zip         INT,
