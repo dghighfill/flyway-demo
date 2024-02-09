6e49e955cd9c34f15f7ec1d664474042bd920576
dale highfill <dhighfi2@jccc.edu>
2024-02-08 18:26:20 -0600

    add toml configuration

--- run_flyway.cmd
+++ run_flyway.cmd
@@ -1,5 +1,6 @@
 ECHO OFF
-set FLYWAY_HOME=I:\desktop-setup\test\DevTools\flyway-9.4.0
+set FLYWAY_HOME=I:\desktop-setup\test\DevTools\flyway-10.7.1
 
 @REM This command will allow paramters to be passed such as info or migrate
-%FLYWAY_HOME%\flyway.cmd -configFiles="./conf/flyway_sqlite.conf" %1 %2 %3 %4
+@REM %FLYWAY_HOME%\flyway.cmd -configFiles="./conf/flyway_sqlite.conf" %1 %2 %3 %4
+%FLYWAY_HOME%\flyway.cmd -configFiles="./conf/flyway_sqlite.toml" %1 %2 %3 %4
