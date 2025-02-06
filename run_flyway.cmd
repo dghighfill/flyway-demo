38aac56f8f656e18a2f8cdcf21e887a5891bbfb6
dale highfill <dhighfi2@jccc.edu>
2025-02-05 19:17:05 -0600

    update command for set

--- run_flyway.cmd
+++ run_flyway.cmd
@@ -1,6 +1,5 @@
 ECHO OFF
-@REM set FLYWAY_HOME=I:\desktop-setup\test\DevTools\flyway-10.7.1
+set FLYWAY_HOME=<INSERT_YOUR_FLYWAY_PATH_HERE>\flyway-10.7.1
 
 @REM This command will allow paramters to be passed such as info or migrate
-@REM %FLYWAY_HOME%\flyway.cmd -configFiles="./conf/flyway_sqlite.conf" %1 %2 %3 %4
 %FLYWAY_HOME%\flyway.cmd -configFiles="./conf/flyway_sqllite.toml" %1 %2 %3 %4
